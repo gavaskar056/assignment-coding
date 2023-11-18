62f9c86fe5ad06047ec7d911f32fea0f4ca0937f
gavaskar056 <95532158+gavaskar056@users.noreply.github.com>
2023-11-18 18:24:26 +0530

    sorting done

---
+++ javascript/sorting.js
@@ -0,0 +1,30 @@
+function sortDesc(array) {
+    let len = array.length;
+
+  
+    for (let i = 0; i < len - 1; i++)
+     {
+        for (let j = i + 1; j < len; j++)
+         {
+             // Checking if the item at present iteration  
+            // is greater than the next iteration
+            if (array[i] < array[j]) {
+                let temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+    return array;
+}
+
+let inputArray = [];
+let inputLength = prompt("Enter the length of the array:");
+
+for (let i = 0; i < inputLength; i++) {
+    let num = parseFloat(prompt(`Enter element ${i + 1}:`));
+    inputArray.push(num);
+}
+
+let sortedArray = sortDesc(inputArray);
+console.log("Sorted Array in Descending Order: ", sortedArray);
